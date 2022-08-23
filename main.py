95caa6f4f7d16cf982d4548ae6e80a569c463606
Damir1108 <71409270+Damir1108@users.noreply.github.com>
2022-08-23 16:13:12 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,102 @@
+import telebot
+import sqlite3
+from threading import Lock
+from telebot import types  # для указание типов
+
+bot = telebot.TeleBot("5570569628:AAGgdIsswRJ0KED4cnoBsSxIJLm48XBpDbg")
+connect = sqlite3.connect("tasks", check_same_thread=False)
+cursor = connect.cursor()
+cursor.execute("""CREATE TABLE IF NOT EXISTS tasks (
+                                        numberTheme STRING, 
+                                        task STRING,
+                                        right_answer STRING,
+                                        theme STRING
+                                    )
+                                                   """)
+connect.commit()
+mutex = Lock()
+listForTask = []
+#
+# cursor.execute("SELECT * FROM tasks")
+# all_results = cursor.fetchall()
+# print(all_results[1][1])
+# print(all_results)
+
+
+@bot.message_handler(commands=["AddTest"])
+def addTest(message):
+    bot.send_message(message.chat.id, "Выберите раздел или тему")
+    test = message.text.split()
+    print(test)
+
+    @bot.message_handler(commands=['razdel'])
+    def add_number_theme(message1):
+        listForTask.append(message1.text.split())
+        print(message1.text.split())
+        bot.send_message(message1.chat.id, "Введите задания ")
+
+    @bot.message_handler(commands=['razdel2'])
+    def add_task(message2):
+        listForTask.append(message2.text.split())
+
+    @bot.message_handler(commands=['razdel3'])
+    def add_right_answer(message3):
+        bot.send_message(message3.chat.id, "Введите номера ответов ")
+        listForTask.append(message3.text.split())
+
+    @bot.message_handler(commands=['razdel4'])
+    def add_theme(message4):
+        listForTask.append(message4.text.split())
+        print(message4.text.split())
+        bot.send_message(message4.chat.id, "Введите тему ")
+
+    @bot.message_handler(commands=["push"])
+    def acept_changes(message5):
+        try:
+            try:
+                mutex.acquire()
+                print(listForTask)
+                update_list_for_task = [listForTask[0][0], listForTask[1][0], listForTask[2][0], listForTask[3][0]]
+                cursor.execute("INSERT INTO tasks VALUES(?, ?, ?, ?);", update_list_for_task)
+                print("its worked")
+                connect.commit()
+            finally:
+                mutex.release()
+        except sqlite3.IntegrityError:
+            pass
+        listForTask.clear()
+
+
+@bot.message_handler(commands=["choose"])
+def choose_theme(message):
+    cursor.execute("SELECT * FROM tasks")
+    all_results = cursor.fetchall()
+    theme = ""
+    task = ""
+    rightAnswer = ""
+    for i in range(len(all_results)):
+        theme += all_results[i][0]
+        theme += " "
+        task += all_results[i][1]
+        task += " "
+        rightAnswer += all_results[i][2]
+        rightAnswer += " "
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    bot.send_message(message.chat.id, "выберете тему \n")
+    bot.send_message(message.chat.id, theme)
+
+    @bot.message_handler(content_types=['text'])
+    def answer_on_theme(message1):
+        for j in range(len(all_results)):
+            print(all_results[i][0])
+            if message1.text == all_results[i][0]:
+                print("ayaya")
+                bot.send_message(message.chat.id, all_results[i][1])
+            if message1.text == all_results[i][2]:
+                bot.send_message(message.chat.id, " right")
+                break
+
+
+
+# def main():
+bot.polling()
